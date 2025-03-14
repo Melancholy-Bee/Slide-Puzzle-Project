c61f8d08b57f175fc503a741cb4fef6613e5d94d
Sylas M. <syborg789@gmail.com>
2025-03-14 14:00:34 -0600

    added quit and fake settings buttons - Sylas

--- mypackage/Menu.java
+++ mypackage/Menu.java
@@ -3,7 +3,6 @@ package mypackage;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Color;
 import java.awt.*;
 
 public class Menu {
@@ -11,7 +10,9 @@ public class Menu {
     static JPanel panel = new JPanel();
     static JButton newGameButton = new JButton("Create New Game");
     static JButton load = new JButton ("Load");
-    
+    static JButton settings = new JButton("Settings");
+    static JButton quit = new JButton("Quit");
+
     public static void initialize(){
         frame.setTitle("Picture This");
         frame.setSize(1000,1000);
@@ -43,6 +44,33 @@ public class Menu {
             }
         });
 
-        //quit and settings buttons Sylas
+        //settings button Sylas
+        settings.setFocusable(false);
+        settings.setFont(new Font("Dialog", Font.PLAIN, 15));
+        settings.setPreferredSize(new Dimension(200, 75));
+        settings.setBackground(new Color(0, 115, 150));
+        panel.add(settings);
+
+        settings.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0); // Closes the program
+            }
+        });
+
+        //quit button sylas
+        quit.setFocusable(false);
+        quit.setFont(new Font("Dialog", Font.PLAIN, 15));
+        quit.setPreferredSize(new Dimension(200, 75));
+        quit.setBackground(new Color(0, 115, 150));
+        panel.add(quit);
+
+        quit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0); // Closes the program
+            }
+        });
+
     }
 }
